4a7c778a079eb715d484b4591d300fd4acc8b103
albertturcu <turcu.albert98@gmail.com>
2019-04-22 17:27:50 +0200

    display qr code

--- home/views.py
+++ home/views.py
@@ -1,15 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template import loader
 from django.views.generic import TemplateView
 import pyqrcode
 import io
 from home.forms import HomeForm
+import markdown 
 
 # Create your views here.
 class index(TemplateView):
     template_name = 'index.html'
-        
+    placeholder_qr = 'https://image.flaticon.com/icons/png/512/107/107072.png'
     def get(self, request):
         form = HomeForm()
         return render(request, self.template_name, {'form': form})
@@ -27,8 +27,15 @@ class index(TemplateView):
         if form.is_valid():
             form.save()
             text = form.cleaned_data['url']
+            
+            qrcode = pyqrcode.create(text)
+            qrcode.png('url.png', scale=5)
+            # print(qrcode.terminal(quiet_zone=1))
+            image_as_str = qrcode.png_as_base64_str(scale=5)
+            html_img = 'data:image/png; base64,{}'.format(image_as_str)
             form = HomeForm()
-            return redirect('/')
+            
+            return render(request, self.template_name, {'qrcode':html_img, 'form': form})
 
         args = {'form': form, 'text': text}
         return render(request, self.template_name, args)
