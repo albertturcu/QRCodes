229b536510cb862c35cd14bd0e10462e9f3ac011
albertturcu <turcu.albert98@gmail.com>
2019-04-22 12:46:50 +0200

    first commit

---
+++ home/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from django.views.generic import TemplateView
+import pyqrcode
+import io
+from home.forms import HomeForm
+
+# Create your views here.
+class index(TemplateView):
+    template_name = 'index.html'
+        
+    def get(self, request):
+        form = HomeForm()
+        return render(request, self.template_name, {'form': form})
+
+    # def generate_url_qrcode(self,request):
+    #     template = loader.get_template('home/index.html')
+    #     img_string = io.StringIO()
+    #     qrcode = pyqrcode.create('google.com')
+    #     qrcode.png(img_string, scale=5)
+    #     # return render(request, 'index.html', img)
+    #     return HttpResponse(template.render(request,body=img_string.getvalue(), content_type="image/png", content_disposition='attachment; filename="url.png"'))
